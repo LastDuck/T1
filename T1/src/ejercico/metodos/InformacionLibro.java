8f8ad831ba06153fc6ca63e9cd37334483415fcb
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-08-13 15:29:30 -0500

    425

--- T1/src/ejercico/metodos/InformacionLibro.java
+++ T1/src/ejercico/metodos/InformacionLibro.java
@@ -1,5 +1,7 @@
 package ejercico.metodos;
 
+import deberes.NoMain1;
+
 public class InformacionLibro {
 
 	public static void main(String[] args) {
@@ -8,6 +10,10 @@ public class InformacionLibro {
 		//trabajo de clase
 		libro l1=new libro();
 		l1.InfLibro("Gary", 25, "terror");
+		
+		//compañero
+		NoMain1 N1=new NoMain1();
+		N1.voleto("pablo", "Alba", "Japon", 2600);
 		
 			}
 		}
