eae5478ad6608990c2e06b81c192d38c00cb0231
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-08-13 14:58:27 -0500

    companero

---
+++ T1/src/ejercico/metodos/InformacionLibro.java
@@ -0,0 +1,12 @@
+package ejercico.metodos;
+
+public class InformacionLibro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		libro l1=new libro();
+		l1.InfLibro("Gary", 25, "terror");
+		
+			}
+		}
--- T1/src/ejercico/metodos/libro.java
+++ T1/src/ejercico/metodos/libro.java
@@ -5,6 +5,14 @@ public class libro {
 	String tipolibro;
 	int AnoLibro;
 	int Costolibro;
+
+	public void InfLibro(String nombredellibro, int Costo,String tipo) {
 	
-	
+	System.out.println("------------------------------");
+	System.out.println("Informacion del libro:");
+	System.out.println("Nombre Del libro:"+nombredellibro);
+	System.out.println("costo:"+Costo+"$");
+	System.out.println("Tipo de libro:"+tipo);
+	System.out.println("------------------------------");
+	}
 }
